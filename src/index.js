3bfc58a90f82efb3130ae6658206398bf2926beb
veer260 <57021124+veer260@users.noreply.github.com>
2022-12-19 20:45:37 +0530

    Completed basic sequelize setup

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const {PORT}  =require('./config/serverConfig')
 
 const setupAndStartServer = async() => {
     const app = express();
     // const PORT = 3000;
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
 
     app.listen(PORT, () => {
         console.log(`server started at port ${PORT}`)
