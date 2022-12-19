62c7063bb93fc584843aac2cdbb0d7897b26d4f5
veer260 <57021124+veer260@users.noreply.github.com>
2022-12-19 19:26:00 +0530

    basic evn setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-const word = "hello world!";
+const express = require('express');
+const {PORT}  =require('./config/serverConfig')
+
+const setupAndStartServer = async() => {
+    const app = express();
+    // const PORT = 3000;
+
+    app.listen(PORT, () => {
+        console.log(`server started at port ${PORT}`)
+    })
+}
+
+setupAndStartServer();
