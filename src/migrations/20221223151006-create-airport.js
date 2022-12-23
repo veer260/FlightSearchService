700f853848e74cc6ebd6301509cb98af989b1576
veer260 <57021124+veer260@users.noreply.github.com>
2022-12-23 20:53:08 +0530

    added airport model

---
+++ src/migrations/20221223151006-create-airport.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const CityRepository = require('../Repository/City-repository');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Airports', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull : false
+      },
+      address: {
+        type: Sequelize.STRING
+      },
+      cityId: {
+        type: Sequelize.INTEGER,
+        references : {
+          model : 'Cities',
+          key : 'id',
+          as : 'cityID'
+        },
+        allowNull : false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Airports');
+  }
+};
